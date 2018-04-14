dac1f511cc2d92384f9c6a3a4d1c59e8414c3f9c
Adam Łamarz <adam.lamarz@outlook.com>
2018-04-14 19:52:58 +0200

    project changes

--- SampleApps/SampleApp.UWP/App.xaml.cs
+++ SampleApps/SampleApp.UWP/App.xaml.cs
@@ -74,7 +74,7 @@ namespace SampleApp.UWP
 				// When the navigation stack isn't restored navigate to the first page,
 				// configuring the new page by passing required information as a navigation
 				// parameter
-				rootFrame.Navigate(typeof(MainPage), e.Arguments);
+				rootFrame.Navigate(typeof(SampleApp.MainPage), e.Arguments);
 			}
 			// Ensure the current window is active
 			Window.Current.Activate();
